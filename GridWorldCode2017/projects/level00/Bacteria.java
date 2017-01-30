729d534ecace2eeed027df6467fb80a3c33bd3de
Maxitothegreat <porterm@SAU-41.NETWORK>
2017-01-30 11:00:39 -0500

    Work

--- GridWorldCode2017/projects/level00/Bacteria.java
+++ GridWorldCode2017/projects/level00/Bacteria.java
@@ -1,7 +1,41 @@
+import java.awt.Color;
+
 import info.gridworld.actor.Actor;
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+
 
 public class Bacteria extends Actor{
 
+	private Location loc = null;
 	
+	public Bacteria()
+	{
+		setColor(Color.BLUE);
+		int col, row = 0;
+		boolean found = false; 
+		while (row < getGrid().getNumRows() && !found) 
+		{ 
+			col = 0;
+			while (col < getGrid().getNumCols() && !found) 
+			{
+				loc = new Location (row, col); 
+				Actor a = getGrid().get(loc); 
+				if (a != null && a instanceof Heart) 
+				{ 
+					found = true; 
+				} 
+				col++; 
+			} 
+			row++; 
+		}
+	}
+	
+	public void act()
+	{
+		setDirection(loc.getDirectionToward(loc));
+		Location next = loc.getAdjacentLocation(getDirection());
+		moveTo(next);
+	}
 	
 }
--- GridWorldCode2017/projects/level00/level00Runner.java
+++ GridWorldCode2017/projects/level00/level00Runner.java
@@ -1,13 +1,12 @@
 import info.gridworld.actor.ActorWorld;
-
 public class level00Runner {
 
 	public static void main(String[] args) {
 		
 		 ActorWorld world = new ActorWorld();
-	        world.add(new Bacteria());
-	        world.add(new Heart());
-	        world.show();
+	     world.add(new Bacteria());
+	     world.add(new Heart());
+	     world.show();
 
 	}
 
